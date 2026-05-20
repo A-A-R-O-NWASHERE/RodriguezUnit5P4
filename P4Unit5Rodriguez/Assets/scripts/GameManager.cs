18140cd9b17c44f44d38cb0e19834315002a1de3
A-A-R-O-NWASHERE <768335@student.dusd.net>
2026-05-20 11:38:08 -0700

    added game start
    now there is a cover for my game when you start it up

--- P4Unit5Rodriguez/Assets/scripts/GameManager.cs
+++ P4Unit5Rodriguez/Assets/scripts/GameManager.cs
@@ -18,10 +18,7 @@ public class GameManager : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        isGameActive = true;
-        StartCoroutine(SpawnTarget());
-        score = 0;
-        UpdateScore(0);
+        
     }
 
     // Update is called once per frame
@@ -56,5 +53,13 @@ public class GameManager : MonoBehaviour
     public void RestartGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void StartGame()
+    {
+        isGameActive = true;
+        StartCoroutine(SpawnTarget());
+        score = 0;
+        UpdateScore(0);
     }
 }
